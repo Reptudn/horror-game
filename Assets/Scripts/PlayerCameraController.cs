81d3d5528a932670b5cecd3fe02f4b59807ff0bd
Tubinex <46114094+Tubinex@users.noreply.github.com>
2022-08-27 22:00:14 +0200

    Implemented New UI & Player Controller

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [Header("Mouse Settings")]
+    [Range(1f, 30f)]
+    public float MouseSensitivity = 15f;
+
+    [Header("Objects")]
+    public GameObject PlayerModel;
+    public GameObject CameraAnchor;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            if (!PlayerModel.activeSelf){ Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false; }
+        }
+
+    }
+
+    private void Update()
+    {
+
+        Camera.main.transform.position = CameraAnchor.transform.position;
+
+        float MouseX = Input.GetAxis("Mouse X") * Time.deltaTime * (MouseSensitivity * 100);
+        float MouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * (MouseSensitivity * 100);
+
+        yRotation += MouseX;
+        xRotation -= MouseY;
+        xRotation  = Mathf.Clamp(xRotation, -90f, 90f);
+
+        Camera.main.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }   
+
+}
