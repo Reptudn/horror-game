334bb8af883f03e5e1c8799f22a283126711200d
Reptudn <87537789+Reptudn@users.noreply.github.com>
2022-08-28 02:14:34 +0200

    fixed player object being disabled when running game

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -24,7 +24,7 @@ public class PlayerMovementController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+       this.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
