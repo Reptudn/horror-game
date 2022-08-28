17f1f156055221761501a922d7caa4a951723764
Tubinex <46114094+Tubinex@users.noreply.github.com>
2022-08-29 01:04:39 +0200

    Implemented Server Browser

---
+++ Assets/Scripts/LobbyDataEntry.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Steamworks;
+using TMPro;
+
+public class LobbyDataEntry : MonoBehaviour
+{
+    [Header("Lobby Data")]
+    public CSteamID LobbyId;
+    public string LobbyName;
+    public int MaxPlayers;
+    public int Players;
+    public CSteamID OwnerId;
+
+    [Header("UI Elements")]
+    public TextMeshProUGUI LobbyNameText;
+    public TextMeshProUGUI PlayerCountText;
+    public RawImage LobbyIcon;
+
+    public void SetLobbyData()
+    {
+
+        LobbyNameText.SetText(LobbyName.Length != 0 ? LobbyName.Split(SteamLobby.LobbyIdentifier)[1] : "Unnamed Lobby");
+        PlayerCountText.SetText(string.Format("({0}/{1})", Players, MaxPlayers != 0 ? MaxPlayers : "--"));
+    }
+
+    public void JoinLobby()
+    {
+
+        SteamLobby.Instance.JoinLobby(LobbyId);
+
+    }
+
+}
---
+++ Assets/Scripts/LobbyListManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class LobbyListManager : MonoBehaviour
+{
+
+    public static LobbyListManager Instance;
+    
+    public GameObject LobbyList;
+    public GameObject LobbyItemPrefab;
+
+    public List<GameObject> Lobbies = new List<GameObject>();
+
+
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+    }
+
+    public void GetLobbies()
+    {
+
+        SteamLobby.Instance.GetLobbiesList();
+
+    }
+
+    public void DestroyLobbies()
+    {
+
+        foreach (GameObject Lobby in Lobbies)
+        {
+
+            Destroy(Lobby);
+
+        }
+        Lobbies.Clear();
+
+    }
+
+    public void DisplayLobbies(List<CSteamID> LobbyIds, LobbyDataUpdate_t result)
+    {
+
+        Debug.Log(LobbyIds.ToString() + ", " + result.ToString());
+        for (int i = 0; i < LobbyIds.Count; i++)
+        {
+
+            if (LobbyIds[i].m_SteamID == result.m_ulSteamIDLobby)
+            {
+                GameObject LobbyItem = Instantiate(LobbyItemPrefab);
+                LobbyItem.GetComponent<LobbyDataEntry>().LobbyId = (CSteamID) LobbyIds[i].m_SteamID;
+                LobbyItem.GetComponent<LobbyDataEntry>().LobbyName = SteamMatchmaking.GetLobbyData((CSteamID) LobbyIds[i].m_SteamID, "name");
+                LobbyItem.GetComponent<LobbyDataEntry>().MaxPlayers = SteamMatchmaking.GetLobbyMemberLimit((CSteamID) LobbyIds[i].m_SteamID);
+                LobbyItem.GetComponent<LobbyDataEntry>().Players = SteamMatchmaking.GetNumLobbyMembers((CSteamID) LobbyIds[i].m_SteamID);
+                LobbyItem.GetComponent<LobbyDataEntry>().OwnerId = SteamMatchmaking.GetLobbyOwner((CSteamID) LobbyIds[i].m_SteamID);
+
+                LobbyItem.GetComponent<LobbyDataEntry>().SetLobbyData();
+
+                LobbyItem.transform.SetParent(LobbyList.transform);
+                LobbyItem.transform.localScale = Vector3.one;
+
+                Lobbies.Add(LobbyItem);
+
+            }
+        }
+    }
+}
