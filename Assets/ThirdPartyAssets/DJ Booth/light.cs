44c90190a349e9cc681358022a7d85011c4fbf1e
Reptudn <87537789+Reptudn@users.noreply.github.com>
2022-08-30 02:46:50 +0200

    fixed issues with the ai

---
+++ Assets/ThirdPartyAssets/DJ Booth/light.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+
+    public Light lightPog;
+
+    public GameObject dancer;
+
+    void Start(){
+        InvokeRepeating("changeLight", 1f, 1.2f);
+        dancer.SetActive(true);
+    }
+    
+    void changeLight(){
+        lightPog.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
+        lightPog.intensity = Random.Range(1, 10);
+    }
+}
