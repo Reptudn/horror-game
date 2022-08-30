e873d6b28242bd0ea83430be2d35dd9e1b47aaab
Reptudn <87537789+Reptudn@users.noreply.github.com>
2022-08-30 23:51:31 +0200

    e

--- Assets/Scripts/World/EasterEggTrigger.cs
+++ Assets/Scripts/World/EasterEggTrigger.cs
@@ -24,7 +24,7 @@ public class EasterEggTrigger : MonoBehaviour
 
     void Update(){
 
-        if(Input.GetKeyDown(KeyCode.F6)) easteregg.SetActive(easteregg.activeSelf);
+        if(Input.GetKeyDown(KeyCode.F6)) easteregg.SetActive(!easteregg.activeSelf);
 
     }
     
