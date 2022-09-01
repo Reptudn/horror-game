193c2c9a2246d7e58edf6a56bc05ba2c6f6252da
Tubinex <46114094+Tubinex@users.noreply.github.com>
2022-09-01 22:14:03 +0200

    Added Equipped Index to Inventory Controller

--- Assets/Scripts/Inventory/PlayerInventoryController.cs
+++ Assets/Scripts/Inventory/PlayerInventoryController.cs
@@ -6,9 +6,43 @@ using Mirror;
 public class PlayerInventoryController : NetworkBehaviour
 {
     
+    public int EquippedIndex = -1;
     public InventoryItem EquippedItem;
+
     [SyncVar(hook = nameof(UpdateInventoryContents))] public List<InventoryItemInstance> Items;
     
+    public void AddItem (InventoryItemInstance Item) 
+    {
+
+        List<InventoryItemInstance> Items = new List<InventoryItemInstance>(this.Items);
+        Items.Add(Item);
+        ChangeInventoryContents(Items);
+
+    }
+
+    public void RemoveItem (InventoryItemInstance Item) 
+    {
+
+        this.Items.Remove(Item);
+
+    }
+
+    private void Update() {
+        
+        if (hasAuthority)
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                if (EquippedIndex < Items.Count - 1) EquippedIndex++;
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                if (EquippedIndex > -1) EquippedIndex--;
+            }
+        }
+
+    }
+
     [Command]
     private void Cmd_UpdateInventoryContents(List<InventoryItemInstance> NewValue)
     {
