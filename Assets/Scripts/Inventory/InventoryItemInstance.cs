70f7bdb5a3d85fc2dd126713cf832e075e6cd792
Tubinex <46114094+Tubinex@users.noreply.github.com>
2022-09-01 02:05:14 +0200

    Added Owner to Item Instance

--- Assets/Scripts/Inventory/InventoryItemInstance.cs
+++ Assets/Scripts/Inventory/InventoryItemInstance.cs
@@ -54,6 +54,8 @@ public class InventoryItemInstance : MonoBehaviour
     }
 
     public InventoryItem Data;
+
+    public PlayerObjectController Owner;
     
     public List<ItemAttribute> Attributes = new List<ItemAttribute>();
 
