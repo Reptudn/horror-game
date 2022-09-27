d78cae88f12b28e469b13a5368d62419ef17d80d
Reptudn <87537789+Reptudn@users.noreply.github.com>
2022-09-27 12:49:30 +0200

    updated generator

--- Assets/Scripts/World Generation/DungeonGenerator.cs
+++ Assets/Scripts/World Generation/DungeonGenerator.cs
@@ -7,7 +7,7 @@ public enum GenerationType{
     Medium,
     Big,
     Huge,
-    Infinite
+    Infinite //Not infinite but nobody is going to know o.o
 }
 
 public class DungeonGenerator : MonoBehaviour
@@ -47,7 +47,60 @@ public class DungeonGenerator : MonoBehaviour
 
     void Generate(int minRooms, int maxRooms){
 
+        int eventRoomsPlaced = 0;
+        int roomsPlaced = 0;
 
+        while(eventRoomsPlaced < eventRooms.Length){
+
+            if(roomsPlaced > maxRooms) break;
+
+            int random = Random.Range(1, 10);
+            if (random < 3) { SpawnEventRoom(eventRooms[eventRoomsPlaced]); eventRoomsPlaced++; }
+            else SpawnRoom(rooms[Random.Range(0, rooms.Length -1)]);
+
+            roomsPlaced++;
+
+        }
+
+    }
+
+    void SpawnEventRoom(GameObject room){
+
+    }
+
+    GameObject lastSpawnedRoom;
+    GameObject[] snappingPoints;
+    void SpawnRoom(GameObject room){
+        if(room == lastSpawnedRoom) { SpawnRoom(rooms[Random.Range(0, rooms.Length -1)]); return; }
+
+        var roomInfo = room.GetComponent<RoomInfo>();
+
+        if(roomInfo == null){
+            Debug.Log("The Room: " + room + " doesnt have a RoomInfo script");
+            return;
+        }
+
+        if(!CanSpawnRoom(room)) { 
+            //hier machen wa was wenn man des ned do hinsetzen kann... for now mucho recursiono
+            SpawnRoom(rooms[Random.Range(0, rooms.Length -1)]); return;
+        } else {
+
+            //da machen wa auch was wenn man des da hinsetzen kann
+
+        }
+        
+        snappingPoints = roomInfo.snappingPoints;
+        lastSpawnedRoom = room;
+    }
+
+    bool CanSpawnRoom(GameObject room){
+        foreach(GameObject o in snappingPoints){
+            if(room.transform.position == o.transform.position) return false;
+        }
+        return true;
+    }
+
+    void SetPosition(){
 
     }
 }
---
+++ Assets/Scripts/World Generation/RoomInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomInfo : MonoBehaviour
+{
+    public string roomName;
+    public GameObject[] snappingPoints;
+    
+}
